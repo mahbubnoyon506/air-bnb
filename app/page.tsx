f2679f60efb74c598f645c16d2ee4909e511946f
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-05-08 12:34:01 +0600

    empty state created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
+
 export default function Home() {
+
+  const isEmpty = true
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <div>
-      <h2 className="text-4xl">Hello Airbnb</h2>
-    </div>
+    <ClientOnly>
+      <Container>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+
+        </div>
+      </Container>
+    </ClientOnly>
   );
 }
